dceb2b0ca97a473ddacba1084eba301fc15daa41
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-01-04 19:23:34 +0900

    Arrays

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let something;
 
 console.log(amIUgly); // something은 변수는 존재하지만 값이 없는 것
 console.log(something); //  null은 변수도 있고 값도 있는데 그 값 자체가 null(비어있음)
+
+const daysOfWeeks = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeeks[4]);
+
+// Add one more day to the Array
+daysOfWeeks.push("sun");
+console.log(daysOfWeeks);
