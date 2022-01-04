0b9f1f5e9ce4c91225b5b2a542bb154b74decb03
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-01-04 18:45:29 +0900

    const, let, var

---
+++ script.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 10;
+
+let myName = "YeongSoo";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log("내 이름은 : " + myName);
+
+myName = "Han";
+console.log("바뀐 내 이름은 : " + myName);
