d78c889e74769edc06905e5a7496231ebb373e4f
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-01-27 00:27:35 +0900

    CSS in JS2

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-
-  if (currentColor === "blue") {
-    newColor = "pink";
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
 }
 
 function handleMouseEnter() {
