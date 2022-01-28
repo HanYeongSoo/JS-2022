eec8130e11543742817a16608dde6327804463cd
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-01-28 18:18:00 +0900

    CSS in JS3

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
+  // const clickedClass = "clicked";
+  // if (h1.className === clickedClass) {
+  //   h1.className = "";
+  // } else {
+  //   h1.className = clickedClass;
+  // }
+
+  // const clickedClass = "clicked";
+  // if (h1.classList.contains(clickedClass)) {
+  //   h1.classList.remove(clickedClass);
+  // } else {
+  //   h1.classList.add(clickedClass);
+  // }
+
   const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  h1.classList.toggle(clickedClass);
 }
 
 function handleMouseEnter() {
