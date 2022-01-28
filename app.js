43369ce3f06f786e84d8c1397ebacc1849b580cd
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-01-28 18:46:46 +0900

    Input Values
    #4.0

--- app.js
+++ app.js
@@ -1,53 +1,8 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginBtn = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-  // const clickedClass = "clicked";
-  // if (h1.className === clickedClass) {
-  //   h1.className = "";
-  // } else {
-  //   h1.className = clickedClass;
-  // }
-
-  // const clickedClass = "clicked";
-  // if (h1.classList.contains(clickedClass)) {
-  //   h1.classList.remove(clickedClass);
-  // } else {
-  //   h1.classList.add(clickedClass);
-  // }
-
-  const clickedClass = "clicked";
-  h1.classList.toggle(clickedClass);
+function onLoginBtnClick() {
+  console.log("click~");
 }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!!!";
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave); // .removeEventListener로 지울 수 있음
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "hotpink";
-}
-
-function handleWIndowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("GooooooD");
-}
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWIndowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+loginBtn.addEventListener("click", onLoginBtnClick);
