6ec93e3ed6dd99cca027f660f3c491cd3a7e6531
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-02-04 17:03:50 +0900

    Loading Username
    #4.6
     .- local storage에 값이 있나? 없나?를 먼저 확인 후 진행
     .- 값이 없으면 form
     .- 값이 있으면 h1 문구

--- app.js
+++ app.js
@@ -3,19 +3,31 @@ const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
 
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
   // greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove("hidden");
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
