817527657f9cf97bc68137575069fc781d6a7ecb
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-02-04 16:31:41 +0900

    Getting Username
    #4.4
     -. form에서 입력 받은 값 숨기고 그 값을 다른 요소에 표현이 가능

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
+
+  const username = loginInput.value;
+
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
-const link = document.querySelector("a");
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  alert("clicked!");
-}
-
-link.addEventListener("click", handleLinkClick);
