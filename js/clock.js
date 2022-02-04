6c7c347320bff49a35498997492d71f3232bacbf
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-02-04 18:48:04 +0900

    Timeouts and Dates
    #5.1
     .- 시간 설정 완료

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,14 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-  console.log("hello~");
+function getClock() {
+  const date = new Date();
+  // 00:00:00 으로 시간을 표현하고 싶은 거니까
+  //   console.log(date.getHours());
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 3000);
+// setInterval(sayHello, 3000);
+// setTimeout(sayHello, 3000);
+
+getClock(); // 페이지가 로딩되자마자 초를 시작하기 위해서 추가
+setInterval(getClock, 1000);
