d409d3bce400e049455fe64dbdb0f74de83a107e
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-02-09 19:55:50 +0900

    Deleting To Dos -01
    #7.6

--- js/todo.js
+++ js/todo.js
@@ -14,11 +14,13 @@ function deleteToDo(event) {
   li.remove();
 }
 
-function paintToDo(newToDo) {
+function paintToDo(newToDoObj) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   li.appendChild(span);
-  span.innerText = newToDo;
+
+  li.id = newToDoObj.id;
+  span.innerText = newToDoObj.text;
 
   const button = document.createElement("button");
   li.appendChild(button);
@@ -39,9 +41,14 @@ function handleToDoSubmit(event) {
   //   console.log("newToDo : " + newToDo);
   //   console.log("toDoInput.value = " + toDoInput.value);
 
-  toDos.push(newToDo);
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
 
-  paintToDo(newToDo);
+  toDos.push(newToDoObj);
+
+  paintToDo(newToDoObj);
   saveToDos();
 }
 
@@ -50,12 +57,12 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 const savedToDos = localStorage.getItem("toDos");
 // console.log(savedToDos);
 
-if (saveToDos !== null) {
+if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   // console.log(parsedToDos);
   // 근데 여기서 받아온 list들을 한 덩어리로 쓰는게 아니고 따로따로 쓰고 싶기 때문에 하나씩 뽑아줘야돼
 
   toDos = parsedToDos;
 
-  parsedToDos.forEach((item) => paintToDo(item));
+  parsedToDos.forEach(paintToDo);
 }
