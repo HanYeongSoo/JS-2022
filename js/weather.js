7b40b3ac6e8fb2fc98a218cea32a7faedb942a1a
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-02-10 17:18:52 +0900

    Weather API
    #8.1

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,22 @@
+const API_KEY = "24f7eda5aaafd611d71e2f0a3413d533";
+
 function ok(GeolocationPosition) {
   //   console.log(GeolocationPosition);
   const lat = GeolocationPosition.coords.latitude;
   const lon = GeolocationPosition.coords.longitude;
-  console.log("당신의 현재 위치는 " + lat + lon);
+  //   console.log("당신의 현재 위치는 " + lat + "?" + lon);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      //   console.log(data.name, data.weather[0].main);
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}도`;
+      city.innerText = data.name;
+    });
 }
 
 function error(params) {
@@ -10,3 +24,6 @@ function error(params) {
 }
 
 navigator.geolocation.getCurrentPosition(ok, error);
+
+// 24f7eda5aaafd611d71e2f0a3413d533
+// https://api.openweathermap.org/data/2.5/weather?lat=37.55609374494766&lon=127.07892076848592&appid=24f7eda5aaafd611d71e2f0a3413d533
